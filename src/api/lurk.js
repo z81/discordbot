c29b3782becfb894ebfbf8488959dd1003778d07
White mage <muzo0@yandex.ru>
2017-09-22 23:05:16 +0300

    escape string

--- src/api/lurk.js
+++ src/api/lurk.js
@@ -1,8 +1,9 @@
 const ogs = require("open-graph-scraper");
+const querystring = require("querystring");
 
 module.exports = q =>
   new Promise((resolve, reject) => {
-    const options = { url: `http://lurkmore.to/${q}` };
+    const options = { url: `http://lurkmore.to/${querystring.escape(q)}` };
     ogs(options, (err, results) => {
       if (err) {
         reject(err);
